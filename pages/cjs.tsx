5ae4eccaeb65a4210be3e7d3b8e200c667d331b7
Yixuan Xu <yixuanxu94@outlook.com>
2022-12-05 21:54:03 +0800

    cjs

---
+++ pages/cjs.tsx
@@ -0,0 +1,11 @@
+import styles from '../styles/Home.module.css'
+import ky from 'ky'
+console.log('ky', ky)
+const evilString = '__esModule'
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      cjs
+    </div>
+  )
+}
---
+++ pages/esm.tsx
@@ -0,0 +1,10 @@
+import styles from '../styles/Home.module.css'
+import ky from 'ky'
+console.log('ky', ky)
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      esm
+    </div>
+  )
+}
